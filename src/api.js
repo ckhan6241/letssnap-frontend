ef72069c9a960b6682c684b9c9dfb6b63eece15f
chan khan <ckhan6241@gmail.com>
2017-09-21 18:39:02 +0800

    wip - login

--- src/api.js
+++ src/api.js
@@ -1,8 +1,16 @@
 const BASE_URL = 'https://www.letssnap.tk'
 const GROUPS_URL = BASE_URL + '/groups'
+const LOGIN_URL = BASE_URL + '/facebook'
+
+function login(callback) {
+  fetch(LOGIN_URL, {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'}
+  }).then(callback)
+}
 
 function createEvent(name, start, stop, hash, callback) {
-  fetch(GROUPS_URL,{
+  fetch(GROUPS_URL, {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({
