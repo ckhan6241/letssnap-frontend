f1545db99cf7eedc6d9e47d9479ffd4fee257813
chan khan <ckhan6241@gmail.com>
2017-09-22 01:46:54 +0800

    added admin page

--- src/api.js
+++ src/api.js
@@ -2,22 +2,18 @@ const BASE_URL = 'https://www.letssnap.tk'
 const GROUPS_URL = BASE_URL + '/groups'
 const LOGIN_URL = BASE_URL + '/facebook'
 
-function login(callback) {
-  fetch(LOGIN_URL, {
-    method: 'GET',
-    headers: {'Content-Type': 'application/json'}
-  }).then(callback)
-}
-
-function createEvent(name, start, stop, hash, callback) {
+function createEvent(name, start, stop, hash, lat, lng, callback) {
   fetch(GROUPS_URL, {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
+    // credentials: 'include',
     body: JSON.stringify({
       name: name,
       start: start,
       stop: stop,
-      hash: hash
+      hash: hash,
+      latitude: lat,
+      longitude: lng
     })
   }).then(callback)
 }
