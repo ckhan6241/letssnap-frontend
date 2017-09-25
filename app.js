51bff4f31022952aa50aba18952004693fd79277
Ana Chua <anaprimawaty@gmail.com>
2017-09-25 18:38:34 +0800

    Serve index.html always

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express()
 
 app.use(express.static(path.join(__dirname, 'build/es5-bundled')))
 
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname, 'build/es5-bundled') + '/index.html')
+})
+
 const server = app.listen(3000, process.env.LETSSNAP_INTERNAL_IP, function() {
     const host = server.address().address
     const port = server.address().port
